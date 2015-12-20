485217b6766ca0298e44fd7dc9c384d963232c39
PolBaladas <polbaladasluna@gmail.com>
2015-12-20 17:02:21 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+import webbrowser
+import os
+from loklak import Loklak
+
+query=raw_input("search query>>")
+print("Retrieving Tweets...")
+l = Loklak()
+tweets=l.search(query)
+
+tweet_struct = "<h5>@username</h5>"+"\n"+"<p class='txt'>txt</h4><br>"+"\n"+"<p>"
+htmlParsedTweets=""
+
+for tweet in tweets['statuses']:
+	htmlTweet = tweet_struct.replace('username', tweet['screen_name']).replace('txt', tweet['text'])
+	htmlParsedTweets = htmlParsedTweets+htmlTweet
+
+origin_htmlContent = None
+with open('index.html', 'r') as file :
+  origin_htmlContent = file.read()
+
+htmlContent = origin_htmlContent.replace('replace', htmlParsedTweets)
+htmlContent = htmlContent.replace('title', query)
+
+with open('results.html', 'w') as file:
+  file.write(htmlContent.encode('utf-8'))
+
+webbrowser.open_new_tab('file://'+os.getcwd()+"/results.html")
